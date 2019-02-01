f066aa560d56ce96ad174848a181da35a07ec65c
SoloGam <44696734+SoloGam@users.noreply.github.com>
2019-02-01 20:48:16 +0200

    Add files via upload

---
+++ Git/hangman_1.php
@@ -0,0 +1,59 @@
+<?php
+  session_start();
+  // session_destroy();
+  if(!isset($_SESSION['word'])) {
+
+  $word = str_split(randomWord());
+  $chances = 5;
+  $guessed = array_fill(0, count($word) -1, '_');
+
+  $_SESSION['word'] = $word;
+
+  $_SESSION['chances'] = $chances;
+
+  $_SESSION['guessed'] = $guessed;
+
+}
+  if (isset($_POST['letter'])) {
+    $letter = $_POST['letter'];
+    if (array_search($letter, $_SESSION['word']) !=false) {
+      foreach($_SESSION['word'] as $key => $value) {
+        if ($value == $letter) {
+          $_SESSION['guessed'][$key] = $letter;
+        }
+      }
+      if (array_search('_', $_SESSION['guessed']) === false) {
+        session_destroy();
+        ?> <script>
+            alert("You Won!");
+        </script> <?php
+      }
+    } else {
+      $_SESSION['chances']--;
+      if ($_SESSION['chances'] == 0) {
+        session_destroy();
+        ?> <script>
+            alert("Game over!");
+        </script> <?php
+      }
+    }
+}
+function randomWord() {
+  $words = file('File.txt');
+  return $words[array_rand($words)];
+}
+?>
+
+<div>
+  Mai ai <?php echo $_SESSION['chances'];?> incercari.
+</div>
+
+<div>
+  <?php echo implode(" ", $_SESSION['guessed']); ?>
+</div>
+
+<form method="post" action="hangman.php">
+  <label>Ghiceste litera</label>
+  <input type="text" name="letter">
+  <input type="submit" name="" value="Send">
+</form>
